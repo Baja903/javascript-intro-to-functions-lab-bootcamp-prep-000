608da1e03c473cfdc119c43c9c6a171341f9c069
rational-dispatch-0384 <baja903@gmail.com>
2019-04-18 18:55:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var uppercase, lowercase;
+
+function shout (string) {
+  return string.toUpperCase ();
+}
+
+function whisper (string) {
+  return string.toLowerCase ();
+}
+
+function logShout(string){
+uppercase=string.toUpperCase();
+string=uppercase;
+console.log (uppercase);
+}
+
+function logWhisper (string){
+  lowercase=string.toLowerCase();
+  string=lowercase;
+  console.log(lowercase);
+}
+
+
+
